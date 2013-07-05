09520fcf527c71f2773b449722ee9226d682a5ce
Thomas <pringle.thomas@gmail.com>
2013-07-05 14:02:02 +0200

    Fixed TEST_LOCALHOST configuration

--- src/main/java/net/whydah/iam/service/config/ApplicationMode.java
+++ src/main/java/net/whydah/iam/service/config/ApplicationMode.java
@@ -9,6 +9,7 @@ public class ApplicationMode {
     public final static String IAM_MODE_KEY = "IAM_MODE";
     public final static String PROD = "PROD";
     public final static String TEST = "TEST";
+    public final static String TEST_L = "TEST_LOCALHOST";
     public final static String DEV = "DEV";
 
     public static String getApplicationMode() {
@@ -20,7 +21,7 @@ public class ApplicationMode {
             System.err.println(IAM_MODE_KEY + " not defined. Must be one of PROD, TEST, DEV.");
             System.exit(4);
         }
-        if(!Arrays.asList(PROD, TEST, DEV).contains(appMode)) {
+        if(!Arrays.asList(PROD, TEST, TEST_L, DEV).contains(appMode)) {
             System.err.println("Unknown " + IAM_MODE_KEY + ": " + appMode);
             System.exit(5);
         }
