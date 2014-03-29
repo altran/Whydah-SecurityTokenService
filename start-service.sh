e48177d32b62cbff7ef82d125902f323ccbd7393
baardl <bard.lind@gmail.com>
2014-03-29 18:40:27 +0100

    Removing old logon info.

--- start-service.sh
+++ start-service.sh
@@ -8,7 +8,7 @@ JARFILE=$A-$V.jar
 
 pkill -f $A
 
-wget --user=altran --password=l1nkSys -O $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=net.whydah.token&a=$A&v=$V&p=jar"
+wget -O $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=net.whydah.token&a=$A&v=$V&p=jar"
 nohup java -jar -DIAM_CONFIG=securitytokenservice.TEST.properties $JARFILE &
 
 
