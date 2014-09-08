7a5de9a9acb60cf073e5724a568baec70df9ec55
Totto <totto@totto.org>
2014-09-08 21:33:03 +0200

    hazelcast config override parameter

--- start-service.sh
+++ start-service.sh
@@ -9,7 +9,7 @@ JARFILE=$A-$V.jar
 pkill -f $A
 
 wget -O $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=net.whydah.token&a=$A&v=$V&p=jar"
-nohup java -jar -DIAM_CONFIG=securitytokenservice.DEV.properties $JARFILE &
+nohup java -jar -DIAM_CONFIG=securitytokenservice.DEV.properties -Dhazelcast.config=hazelcast.xml $JARFILE &
 
 
 tail -f nohup.out
