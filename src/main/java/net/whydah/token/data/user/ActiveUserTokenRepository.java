ca5a23449748644b56a9738da4fb8c42e6998c4e
Totto <totto@totto.org>
2014-09-22 21:02:48 +0200

    configuration management of hazelcast overrides and tests

--- src/main/java/net/whydah/token/data/user/ActiveUserTokenRepository.java
+++ src/main/java/net/whydah/token/data/user/ActiveUserTokenRepository.java
@@ -1,22 +1,35 @@
 package net.whydah.token.data.user;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 public class ActiveUserTokenRepository {
     private final static Logger logger = LoggerFactory.getLogger(ActiveUserTokenRepository.class);
+    private static Map<String, UserToken> activeusertokensmap;
 
-    private static Config configApp1 = new Config();
-    HazelcastInstance h1 = Hazelcast.newHazelcastInstance(configApp1);
+    static {
+        String xmlFileName = System.getProperty("hazelcast.config");
+        Config configApp1 = new Config();
+        if (xmlFileName != null && xmlFileName.length() > 10) {
+            try {
+                configApp1 = new XmlConfigBuilder(xmlFileName).build();
+                logger.info("Loading hazelcast configuration from :" + xmlFileName);
+            } catch (FileNotFoundException notFound) {
+                logger.error("Error - not able to load hazelcast.xml configuration.  Using embedded as fallback");
+            }
+        }
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(configApp1);
+        activeusertokensmap = hazelcastInstance.getMap("activeusertokensmap");
 
-    private static HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(configApp1);
-    private static Map<String, UserToken> tokens = hazelcastInstance.getMap("tokens");
-    //private static final Map<String, UserToken> tokens = new ConcurrentHashMap<String, UserToken>(1024);
+    }
+
 
     /**
      * Get UserToken from UserTokenRepository. If token is not found or is not valid/timed out, null is returned.
@@ -29,7 +42,7 @@ public class ActiveUserTokenRepository {
         if (tokenId == null) {
             return null;
         }
-        UserToken resToken = tokens.get(tokenId);
+        UserToken resToken = activeusertokensmap.get(tokenId);
         if (resToken != null && verifyUserToken(resToken)) {
             logger.info("Valid userToken found: " + resToken);
             logger.debug("userToken=" + resToken);
@@ -50,7 +63,7 @@ public class ActiveUserTokenRepository {
             logger.info("UserToken not valid, missing tokenId");
             return false;
         }
-        UserToken resToken = tokens.get(token.getTokenid());
+        UserToken resToken = activeusertokensmap.get(token.getTokenid());
         if (resToken == null) {
             logger.info("UserToken not found in repo.");
             return false;
@@ -58,7 +71,7 @@ public class ActiveUserTokenRepository {
         logger.debug("UserToken from repo: {}", resToken);
         if (!resToken.isValid()) {
             logger.debug("resToken is not valid");
-            tokens.remove(token.getTokenid());
+            activeusertokensmap.remove(token.getTokenid());
             return false;
         }
         if (token.toString().equals(resToken.toString())) {
@@ -73,16 +86,16 @@ public class ActiveUserTokenRepository {
             logger.error("Error: token has net tokenid");
             return;
         }
-        if (tokens.containsKey(token.getTokenid())) {
+        if (activeusertokensmap.containsKey(token.getTokenid())) {
             logger.error("Error: trying to update an already existing UserToken in repo..");
             return;
         }
         UserToken copy = token.copy();
-        tokens.put(copy.getTokenid(), copy);
+        activeusertokensmap.put(copy.getTokenid(), copy);
         logger.info("Added token with id {}", copy.getTokenid(), " content:" + copy);
     }
 
     public static void removeUserToken(String userTokenId) {
-        tokens.remove(userTokenId);
+        activeusertokensmap.remove(userTokenId);
     }
 }
