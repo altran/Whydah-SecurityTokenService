9d8efe29d49d49c2bc2329ff7cb997b1d65747c3
Totto <totto@totto.org>
2014-10-24 09:26:29 +0200

    hystrix circuit-breaker added

---
+++ src/main/java/net/whydah/token/data/user/commands/CommandUIBAuth.java
@@ -0,0 +1,20 @@
+package net.whydah.token.data.user.commands;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+public class CommandUIBAuth extends HystrixCommand<String> {
+
+    private final String name;
+
+    public CommandUIBAuth(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
+        this.name = name;
+    }
+
+    @Override
+    protected String run() {
+        return "Hello " + name + "!";
+    }
+}
+
