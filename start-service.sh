40190343a21636764afa3c73e569e1b9862d2ba6
Totto <totto@totto.org>
2015-01-10 22:22:30 +0100

    added detatch :)

--- start-service.sh
+++ start-service.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup /usr/bin/java -DIAM_MODE=PROD -Dhazelcast.config=hazelcast.xml -DIAM_CONFIG=/home/SecurityTokenService/securitytokenservice.PROD.properties -jar /home/SecurityTokenService/SecurityTokenService.jar
+nohup /usr/bin/java -DIAM_MODE=PROD -Dhazelcast.config=hazelcast.xml -DIAM_CONFIG=/home/SecurityTokenService/securitytokenservice.PROD.properties -jar /home/SecurityTokenService/SecurityTokenService.jar &
