8386c7a03605f5ba72db65c4bc3f4fce2a1b2725
Thor Henning Hetland <totto@totto.org>
2015-04-14 16:17:56 +0200

    parametrized

--- start-service.sh
+++ start-service.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
-nohup /usr/bin/java -DIAM_MODE=PROD -Dhazelcast.config=hazelcast.xml -DIAM_CONFIG=/home/SecurityTokenService/securitytokenservice.PROD.properties -jar /home/SecurityTokenService/SecurityTokenService.jar &
+
+#  If IAM_MODE not set, use PROD
+if [ -z "$IAM_MODE" ]
+  IAM_MODE=PROD
+fi
+
+# If IAM_CONFOG not set, use PROD properties from local directory
+if [ -z "$IAM_CONFIG" ]
+  IAM_CONFIG=securitytokenservice.PROD.properties
+fi
+
+# If Version is from source, find the artifact
+if [ $Version = "FROM_SOURCE" ]; then 
+    # Find the bult artifact
+    Version=$(find target/* -name *.jar | grep SNAPSHOT | grep -v original | gre
+p -v lib)
+else
+    Version=SecurityTokenService.jar
+fi
+
+
+nohup /usr/bin/java -DIAM_MODE=$IAM_MODE -Dhazelcast.config=hazelcast.xml -DIAM_CONFIG=$IAM_CONFIG -jar  $Version
+
