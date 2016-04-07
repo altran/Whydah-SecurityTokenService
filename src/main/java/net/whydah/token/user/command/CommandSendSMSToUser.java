9a1bed604beca071d8e2eb66c2be918228222406
Thor Henning Hetland <totto@totto.org>
2016-04-07 22:15:26 +0200

    added robustness in case of missing params

--- src/main/java/net/whydah/token/user/command/CommandSendSMSToUser.java
+++ src/main/java/net/whydah/token/user/command/CommandSendSMSToUser.java
@@ -28,6 +28,9 @@ public class CommandSendSMSToUser extends HystrixCommand<String> {
         replacedStr = replacedStr.replaceAll("smsserviceusername", username);
         replacedStr = replacedStr.replaceAll("smsservicepassword", password);
         this.queryparam = replacedStr.replaceAll("smscontent", smsMessage.replaceAll(" ", "%20"));
+        if (smsMessage == null || cellNo == null || serviceUrl == null || queryparam == null) {
+            log.error("CommandSendSMSToUser initialized with null-values - will fail - smsMessage:{}, cellNo:{}, serviceUrl:{}, queryparam:{}", smsMessage, cellNo, serviceUrl, queryparam);
+        }
     }
 
     @Override
