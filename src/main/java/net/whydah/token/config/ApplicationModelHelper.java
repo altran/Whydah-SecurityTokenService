19cab1e0cc069412717c273aed2867891aea8f91
Thor Henning Hetland <totto@totto.org>
2016-05-03 13:30:43 +0200

    Begun work on a convenient way of using properties from the managed application model

---
+++ src/main/java/net/whydah/token/config/ApplicationModelHelper.java
@@ -0,0 +1,40 @@
+package net.whydah.token.config;
+
+import net.whydah.sso.user.mappers.UserTokenMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+
+public class ApplicationModelHelper {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationModelHelper.class);
+    private static String myApplicationList = "[{}]";
+
+    public static String getApplicationList() {
+        return myApplicationList;
+    }
+
+    public static void setApplicationList(String appLinks) {
+        myApplicationList = appLinks;
+    }
+
+    public static void updateApplicationList(URI userAdminServiceUri, String myAppTokenId, String userTokenXML) {
+        if (shouldUpdate() || getApplicationList() == null || myApplicationList.length() < 6) {
+            String userTokenId = UserTokenMapper.fromUserTokenXml(userTokenXML).getTokenid();
+            String applicationsJson = new net.whydah.sso.commands.adminapi.application.CommandListApplications(userAdminServiceUri, myAppTokenId, userTokenId, "").execute();
+            log.debug("AppLications returned:" + applicationsJson);
+            if (applicationsJson != null) {
+                if (applicationsJson.length() > 20) {
+                    setApplicationList(applicationsJson);
+                }
+            }
+        }
+    }
+
+
+    public static boolean shouldUpdate() {
+        int max = 1000;
+        return (5 >= ((int) (Math.random() * max)));  // update on 0.5 percent of requests
+    }
+
+}
