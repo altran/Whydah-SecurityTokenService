c5506261f937417ded4297a2350b1ac3de65e3f8
Thor Henning Hetland <totto@totto.org>
2016-05-07 23:44:45 +0200

    SDK 2.1.172

--- src/main/java/net/whydah/token/config/ApplicationModelHelper.java
+++ src/main/java/net/whydah/token/config/ApplicationModelHelper.java
@@ -1,8 +1,6 @@
 package net.whydah.token.config;
 
-import net.whydah.sso.application.mappers.ApplicationMapper;
 import net.whydah.sso.application.types.Application;
-import net.whydah.sso.basehelpers.JsonPathHelper;
 import net.whydah.sso.util.ApplicationModelUtil;
 import java.net.URI;
 import java.util.List;
@@ -18,15 +16,12 @@ public class ApplicationModelHelper {
     }
 
     public static Application getApplication(String applicationID) {
-        for (Application application : getApplicationList()) {
-            return application;
-        }
-        return null;
+        ApplicationModelUtil.getApplication(applicationID);
     }
 
     // TODO temp override
     public static List<Application> getApplicationList() {
-        return getApplicationList();
+        return ApplicationModelUtil.getApplicationList();
     }
 
     // JsonPath query against Application.json to find value, empty string if not found
