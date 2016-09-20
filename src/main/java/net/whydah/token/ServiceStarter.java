2ad851afad19da99b5228c393afc8e183d597232
huy <huy@HUY-THINKPAD>
2016-09-20 17:56:49 +0700

    update to the latest versions for guice/grizzly/jersey

--- src/main/java/net/whydah/token/ServiceStarter.java
+++ src/main/java/net/whydah/token/ServiceStarter.java
@@ -1,23 +1,25 @@
 package net.whydah.token;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.servlet.GuiceFilter;
-import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import java.io.IOException;
+
 import net.whydah.sso.config.ApplicationMode;
 import net.whydah.token.config.AppConfig;
 import net.whydah.token.config.SecurityTokenServiceModule;
 import net.whydah.token.user.ActiveUserTokenRepository;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
-import org.glassfish.grizzly.http.server.ServerConfiguration;
-import org.glassfish.grizzly.servlet.ServletHandler;
+import org.glassfish.grizzly.servlet.ServletRegistration;
+import org.glassfish.grizzly.servlet.WebappContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.valuereporter.agent.activity.ObservedActivityDistributer;
 import org.valuereporter.agent.http.HttpObservationDistributer;
 
-import java.io.IOException;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceFilter;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 
 public class ServiceStarter {
     private static final Logger log = LoggerFactory.getLogger(ServiceStarter.class);
@@ -57,26 +59,51 @@ public class ServiceStarter {
         new Thread(new ObservedActivityDistributer(reporterHost, reporterPort, prefix, cacheSize, forwardInterval)).start();
         new Thread(new HttpObservationDistributer(reporterHost, reporterPort, prefix)).start();
 
+        
+        
+     
+      
+        //HUYDO: this is the old implementation which will break when grizzly dependencies are updated 
+        
+//		  ServletHandler adapter = new ServletHandler();
+//        adapter.setContextPath(CONTEXTPATH);
+//        adapter.addInitParameter("com.sun.jersey.config.property.packages", "net.whydah");
+//        adapter.setProperty(ServletHandler.LOAD_ON_STARTUP, "1");
+//
+//        
+//        GuiceFilter filter = new GuiceFilter();
+//        adapter.addFilter(filter, "guiceFilter", null);
+//       
+//        GuiceContainer container = new GuiceContainer(injector);
+//        adapter.setServletInstance(container);
+       
+        
+        //HUYDO: new implementation
 
-        ServletHandler adapter = new ServletHandler();
-        adapter.setContextPath(CONTEXTPATH);
-        adapter.addInitParameter("com.sun.jersey.config.property.packages", "net.whydah");
-        adapter.setProperty(ServletHandler.LOAD_ON_STARTUP, "1");
-
-        GuiceFilter filter = new GuiceFilter();
-        adapter.addFilter(filter, "guiceFilter", null);
-
+        final WebappContext context = new WebappContext("grizzly", CONTEXTPATH);
         GuiceContainer container = new GuiceContainer(injector);
-        adapter.setServletInstance(container);
-
+        final ServletRegistration servletRegistration = context.addServlet("ServletContainer", container);
+        servletRegistration.addMapping("/*");
+        servletRegistration.setInitParameter("com.sun.jersey.config.property.packages", "net.whydah");
+        context.addFilter("guiceFilter", GuiceFilter.class);
+        
+      
         webappPort = Integer.valueOf(appConfig.getProperty("service.port"));
         httpServer = new HttpServer();
-        ServerConfiguration serverconfig = httpServer.getServerConfiguration();
-        serverconfig.addHttpHandler(adapter, "/");
-        NetworkListener listener = new NetworkListener("grizzly", NetworkListener.DEFAULT_NETWORK_HOST, webappPort);
+        //ServerConfiguration serverconfig = httpServer.getServerConfiguration();
+        //serverconfig.addHttpHandler(handler, "/");
+        NetworkListener listener = new NetworkListener("grizzly server", NetworkListener.DEFAULT_NETWORK_HOST, webappPort);
         httpServer.addListener(listener);
+        
+       
+        context.deploy(httpServer);
+        
         httpServer.start();
-
+        
+        
+        
+        
+        
         ActiveUserTokenRepository.initializeDistributedMap();  // Kick-off hazelcast distributed tokensession-map
 
         log.info("SecurityTokenService started on port {}, IAM_MODE = {}", webappPort, ApplicationMode.getApplicationMode());
