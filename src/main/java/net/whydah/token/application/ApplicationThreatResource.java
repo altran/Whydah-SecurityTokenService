4a83c129662e6da53ef252f19d3f0e4e8b279f8d
totto <totto@totto.org>
2017-08-17 17:57:38 +0200

    type-hendling if it exist in threatsignal resource

--- src/main/java/net/whydah/token/application/ApplicationThreatResource.java
+++ src/main/java/net/whydah/token/application/ApplicationThreatResource.java
@@ -1,7 +1,9 @@
 package net.whydah.token.application;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.whydah.admin.health.HealthResource;
 import net.whydah.sso.whydah.DEFCON;
+import net.whydah.sso.whydah.ThreatSignal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +15,7 @@ import java.time.Instant;
 @Path("/threat")
 public class ApplicationThreatResource {
     private final static Logger log = LoggerFactory.getLogger(ApplicationThreatResource.class);
-
+    private static final ObjectMapper mapper = new ObjectMapper();
     private static String defconvalue= DEFCON.DEFCON5.toString();
 
 
@@ -23,13 +25,34 @@ public class ApplicationThreatResource {
     public Response logSignal(@PathParam("applicationtokenid") String applicationtokenid,
                               @FormParam("signal") String jsonSignal) {
         log.warn("logSignal with applicationtokenid: {} - signal={}", applicationtokenid, jsonSignal);
-        if (applicationtokenid != null && applicationtokenid.length() > 6) {
-            String applicationID = AuthenticatedApplicationRepository.getApplicationIdFromApplicationTokenID(applicationtokenid);
-            String applicationName = AuthenticatedApplicationRepository.getApplicationNameFromApplicationTokenID(applicationtokenid);
-            HealthResource.addThreatSignal(applicationtokenid + " (" + applicationID + "[" + applicationName + "]):" + jsonSignal + " - Received: " + Instant.now());
-        } else {  // Allow and handle threat signals from non-whydah components
-            HealthResource.addThreatSignal(applicationtokenid + " ([]):" + jsonSignal + " - Received: " + Instant.now());
+
+        ThreatSignal receivedSignal;
+        String formattedSignal;
+        try {
+
+            receivedSignal = mapper.readValue(jsonSignal, ThreatSignal.class);
+            if (receivedSignal.getSignalEmitter() != null || receivedSignal.getSignalEmitter().length() < 5) {
+                if (applicationtokenid != null && applicationtokenid.length() > 6) {
+                    String applicationID = AuthenticatedApplicationRepository.getApplicationIdFromApplicationTokenID(applicationtokenid);
+                    String applicationName = AuthenticatedApplicationRepository.getApplicationNameFromApplicationTokenID(applicationtokenid);
+                    receivedSignal.setSignalEmitter(applicationID + ":" + applicationName);
+                }
+            }
+        } catch (Exception e) {
+            receivedSignal = new ThreatSignal();
+            receivedSignal.setText(jsonSignal);
+            if (applicationtokenid != null && applicationtokenid.length() > 6) {
+                String applicationID = AuthenticatedApplicationRepository.getApplicationIdFromApplicationTokenID(applicationtokenid);
+                String applicationName = AuthenticatedApplicationRepository.getApplicationNameFromApplicationTokenID(applicationtokenid);
+                receivedSignal.setSignalEmitter(applicationID + ":" + applicationName);
+            }
         }
+        try {
+            formattedSignal = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(receivedSignal);
+        } catch (Exception e) {
+            formattedSignal = jsonSignal;
+        }
+        HealthResource.addThreatSignal(applicationtokenid + " (" + formattedSignal + ") - Received: " + Instant.now());
         return Response.ok().build();
     }
 
