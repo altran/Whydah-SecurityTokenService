caf4adf4dc82844d8c3bdbbfffa6203e4bd7d842
totto <totto@totto.org>
2017-09-22 20:58:49 +0200

    minor steps

--- src/main/java/net/whydah/token/application/AuthenticatedApplicationTokenRepository.java
+++ src/main/java/net/whydah/token/application/AuthenticatedApplicationTokenRepository.java
@@ -75,7 +75,7 @@ public class AuthenticatedApplicationTokenRepository {
                 // Maybe update key here...
             } else {
                 // Bootstrap key initialization
-                ExchangeableKey applicationKey = new ExchangeableKey(applicationToken.getApplicationTokenId().getBytes(), new IvParameterSpec("01234567890ABCDEF".getBytes()));
+                ExchangeableKey applicationKey = new ExchangeableKey(applicationToken.getApplicationTokenId().getBytes(), new IvParameterSpec("01234567890ABCDE".getBytes()));
                 applicationKeyMap.put(applicationToken.getApplicationTokenId(), applicationKey.toJsonEncoded());
             }
         }
